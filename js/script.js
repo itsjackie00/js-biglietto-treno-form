20ce5b941f0c2f7d92a014ddb305df48d635063a
itsjackie00 <jackiedanso33@gmail.com>
2024-03-07 14:58:35 +0100

    assigment

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* alert('funziona?'); */
+
+// ASSIGMENT
+/*
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale 
+del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+*/
+
+/*
+// OBBIETIVO
+- Calcolare il prezzo del biglietto (userKm * 0.21)
+- Stampare un messaggio col prezzo sulla pagina
+
+VARIABILI
+const priceKm = 0.21;
+let userKm = parseInt('Quanti chilometri vuoi percorre ?');
+let price = priceKm * userKm;
+
+let ticketEl = document.getElementById('ticket')
+let message;
+
+let scontoJr = 20 / 100;
+let scontoSr = 40 / 100;
+calcolo (price - price * discount)
+
+let age = parseint("Inserisci l'età ..");
+se l'utente ha meno di 18 anni scontoJr se ha almeno 65 anni scontoSr
+altrimenti prezzo pieno
